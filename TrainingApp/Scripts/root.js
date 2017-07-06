ac8163dff748f5493bf78b3e63d8caf267ecfb89
pkahlau <pkahlau@insigma.de>
2017-07-06 14:36:49 +0200

    removed javascript from the root_page.html and put it in the root.js

--- TrainingApp/Scripts/root.js
+++ TrainingApp/Scripts/root.js
@@ -6,3 +6,8 @@
 //if ($('#qunitTest').length) {
 //  $getScript('/Scripts/qunit.js');
 //}
+
+$(document).ready(function () {
+  loadContentData('/Views/Navbar.html', 'nav[data-portal-navbar]');
+  //loadContentData('error/test.html', 'header', '#errorBox', '<strong><i class="fa fa-exclamation-triangle"></i>&nbsp;Achtung:</strong>&nbsp;Beim Laden ist ein Fehler aufgetreten.')
+});
