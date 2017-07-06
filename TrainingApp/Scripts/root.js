f277b14e943e6295aabbf2e4005d1fd1da8f7da2
pkahlau <pkahlau@insigma.de>
2017-07-06 14:50:48 +0200

    removed old javascript

--- TrainingApp/Scripts/root.js
+++ TrainingApp/Scripts/root.js
@@ -1,13 +1,4 @@
-﻿// include all script files
-//$.getScript('/Scripts/bootstrap.min.js');
-//$.getScript('/Scripts/Custom/navbar.js');
-
-// conditional includes
-//if ($('#qunitTest').length) {
-//  $getScript('/Scripts/qunit.js');
-//}
-
-$(document).ready(function () {
+﻿$(document).ready(function () {
   loadContentData('/Views/Navbar.html', 'nav[data-portal-navbar]');
   //loadContentData('error/test.html', 'header', '#errorBox', '<strong><i class="fa fa-exclamation-triangle"></i>&nbsp;Achtung:</strong>&nbsp;Beim Laden ist ein Fehler aufgetreten.')
 });
