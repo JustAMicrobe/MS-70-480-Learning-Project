5748005f3b211babbda292dff8683fb9d1fe3ef7
pkahlau <pkahlau@insigma.de>
2017-07-31 12:22:21 +0200

    css changes

--- TrainingApp/Scripts/root.js
+++ TrainingApp/Scripts/root.js
@@ -1,4 +1,4 @@
 ﻿$(document).ready(function () {
   loadContentData('/Views/Navbar.html', 'nav[data-portal-navbar]');
-  //loadContentData('error/test.html', 'header', '#errorBox', '<strong><i class="fa fa-exclamation-triangle"></i>&nbsp;Achtung:</strong>&nbsp;Beim Laden ist ein Fehler aufgetreten.')
+  loadContentData('error/test.html', 'header', '#errorBox', '<strong><i class="fa fa-exclamation-triangle"></i>&nbsp;Achtung:</strong>&nbsp;Beim Laden ist ein Fehler aufgetreten.')
 });
