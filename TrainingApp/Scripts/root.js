6c9563777d2a62a4ef63eb13cd33e5f14e3de441
pkahlau <pkahlau@insigma.de>
2017-07-31 15:38:14 +0200

    added views, loading signs in root html and javascrit calls

--- TrainingApp/Scripts/root.js
+++ TrainingApp/Scripts/root.js
@@ -1,5 +1,7 @@
 ﻿$(document).ready(function () {
-  loadContentData('/Views/Navbar.html', 'nav[data-portal-navbar]');
-  loadContentData('/Views/Home.html', 'section[data-portal-content]', '#errorBox');
+  loadContentData('/Views/Header/_Navbar.html', 'nav[data-portal-function="navbar"]', '#errorBox');
+  loadContentData('/Views/Header/_PageTitle.html', 'header[data-portal-function="header"] hgroup', '#errorBox');
+  loadContentData('/Views/Content/_Home.html', 'section[data-portal-function="content"]', '#errorBox');
+  loadContentData('/Views/Footer/_Footer.html', 'footer[data-portal-function="footer"]', '#errorBox');
   //loadContentData('error/test.html', 'header', '#errorBox', '<strong><i class="fa fa-exclamation-triangle"></i>&nbsp;Achtung:</strong>&nbsp;Beim Laden ist ein Fehler aufgetreten.')
 });
